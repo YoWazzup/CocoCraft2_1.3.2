660a97e01fa81777220d151320eda9ef344f1257
YoWazzup <kaaskraker@hotmail.nl>
2012-08-27 16:03:39 +0200

    Messed code up, used roll back to fix. Added configurable item ids.

--- minecraft/cococraft/common/CocoCraft.java
+++ minecraft/cococraft/common/CocoCraft.java
@@ -39,6 +39,35 @@ public class CocoCraft
 	static int SilverOreID;
 	static int AmethystOreID;
 	
+	public static int AmethystID;
+	public static int CocoIngotID;
+	public static int MithrilIngotID;
+	public static int SilverIngotID;
+
+	public static int CocoPickaxeID;
+	public static int CocoAxeID;
+	public static int CocoShovelID;
+	public static int CocoSwordID;
+	public static int CocoHoeID;
+
+	public static int MithrilPickaxeID;
+	public static int MithrilAxeID;
+	public static int MithrilShovelID;
+	public static int MithrilSwordID;
+	public static int MithrilHoeID;
+
+	public static int SilverPickaxeID;
+	public static int SilverAxeID;
+	public static int SilverShovelID;
+	public static int SilverSwordID;
+	public static int SilverHoeID;
+
+	public static int AmethystPickaxeID;
+	public static int AmethystAxeID;
+	public static int AmethystShovelID;
+	public static int AmethystSwordID;
+	public static int AmethystHoeID;
+	
 	@SidedProxy(clientSide = "cococraft.client.ClientProxyCocoCraft", serverSide = "cococraft.common.CommonProxyCocoCraft")
 	public static CommonProxyCocoCraft proxy;
 	public static GameRegistry gr;
@@ -62,41 +91,41 @@ public class CocoCraft
 
 
 	//Items
-	public static final Item AmethystGem = (new ModItem(5000)).setItemName("AmethystGem").setIconCoord(0,0);
-	public static final Item CocoIngot = (new ModItem(5001)).setItemName("CocoIngot").setIconCoord(1,0);
-	public static final Item MithrilIngot = (new ModItem(5002)).setItemName("MithrilIngot").setIconCoord(2,0);
-	public static final Item SilverIngot = (new ModItem(5003)).setItemName("SilverIngot").setIconCoord(3,0);
+		public static final Item AmethystGem = (new ModItem(AmethystID)).setItemName("AmethystGem").setIconCoord(0,0);
+		public static final Item CocoIngot = (new ModItem(CocoIngotID)).setItemName("CocoIngot").setIconCoord(1,0);
+		public static final Item MithrilIngot = (new ModItem(MithrilIngotID)).setItemName("MithrilIngot").setIconCoord(2,0);
+		public static final Item SilverIngot = (new ModItem(SilverIngotID)).setItemName("SilverIngot").setIconCoord(3,0);
 
-	//Tools
-	public static final Item CocoPickaxe = (new ModItemPick(5004, toolCoco)).setIconCoord(0,1).setItemName("CocoPick");
-	public static final Item CocoAxe = (new ModItemAxe(5005, toolCoco)).setIconCoord(1, 1).setItemName("CocoAxe");
-	public static final Item CocoShovel = (new ModItemShovel(5006, toolCoco)).setIconCoord(2,1).setItemName("CocoShovel");
-	public static final Item CocoSword = (new ModItemSword(5007, toolCoco)).setIconCoord(3,1).setItemName("CocoSword");
-	public static final Item CocoHoe = (new ModItemHoe(5008, toolCoco)).setIconCoord(4,1).setItemName("CocoHoe");
+		//Tools
+		public static final Item CocoPickaxe = (new ModItemPick(CocoPickaxeID, toolCoco)).setIconCoord(0,1).setItemName("CocoPick");
+		public static final Item CocoAxe = (new ModItemAxe(CocoAxeID, toolCoco)).setIconCoord(1, 1).setItemName("CocoAxe");
+		public static final Item CocoShovel = (new ModItemShovel(CocoShovelID, toolCoco)).setIconCoord(2,1).setItemName("CocoShovel");
+		public static final Item CocoSword = (new ModItemSword(CocoSwordID, toolCoco)).setIconCoord(3,1).setItemName("CocoSword");
+		public static final Item CocoHoe = (new ModItemHoe(CocoHoeID, toolCoco)).setIconCoord(4,1).setItemName("CocoHoe");
 
-	public static final Item MithrilPickaxe = (new ModItemPick(5009, toolMithril)).setIconCoord(5,1).setItemName("MithrilPick");
-	public static final Item MithrilAxe = (new ModItemAxe(5010, toolMithril)).setIconCoord(6, 1).setItemName("MithrilAxe");
-	public static final Item MithrilShovel = (new ModItemShovel(5011, toolMithril)).setIconCoord(7,1).setItemName("MithrilShovel");
-	public static final Item MithrilSword = (new ModItemSword(5012, toolMithril)).setIconCoord(8,1).setItemName("MithrilSword");
-	public static final Item MithrilHoe = (new ModItemHoe(5013, toolMithril)).setIconCoord(9,1).setItemName("MithrilHoe");
+		public static final Item MithrilPickaxe = (new ModItemPick(MithrilPickaxeID, toolMithril)).setIconCoord(5,1).setItemName("MithrilPick");
+		public static final Item MithrilAxe = (new ModItemAxe(MithrilAxeID, toolMithril)).setIconCoord(6, 1).setItemName("MithrilAxe");
+		public static final Item MithrilShovel = (new ModItemShovel(MithrilShovelID, toolMithril)).setIconCoord(7,1).setItemName("MithrilShovel");
+		public static final Item MithrilSword = (new ModItemSword(MithrilSwordID, toolMithril)).setIconCoord(8,1).setItemName("MithrilSword");
+		public static final Item MithrilHoe = (new ModItemHoe(MithrilHoeID, toolMithril)).setIconCoord(9,1).setItemName("MithrilHoe");
 
-	public static final Item SilverPickaxe = (new ModItemPick(5014, toolSilver)).setIconCoord(10,1).setItemName("SilverPick");
-	public static final Item SilverAxe = (new ModItemAxe(5015, toolSilver)).setIconCoord(11, 1).setItemName("SilverAxe");
-	public static final Item SilverShovel = (new ModItemShovel(5016, toolSilver)).setIconCoord(12,1).setItemName("SilverShovel");
-	public static final Item SilverSword = (new ModItemSword(5017, toolSilver)).setIconCoord(13,1).setItemName("SilverSword");
-	public static final Item SilverHoe = (new ModItemHoe(5018, toolSilver)).setIconCoord(14,1).setItemName("SilverHoe");
+		public static final Item SilverPickaxe = (new ModItemPick(SilverPickaxeID, toolSilver)).setIconCoord(10,1).setItemName("SilverPick");
+		public static final Item SilverAxe = (new ModItemAxe(SilverAxeID, toolSilver)).setIconCoord(11, 1).setItemName("SilverAxe");
+		public static final Item SilverShovel = (new ModItemShovel(SilverShovelID, toolSilver)).setIconCoord(12,1).setItemName("SilverShovel");
+		public static final Item SilverSword = (new ModItemSword(SilverSwordID, toolSilver)).setIconCoord(13,1).setItemName("SilverSword");
+		public static final Item SilverHoe = (new ModItemHoe(SilverHoeID, toolSilver)).setIconCoord(14,1).setItemName("SilverHoe");
 
-	public static final Item AmethystPickaxe = (new ModItemPick(5019, toolAmethyst)).setIconCoord(15,1).setItemName("AmethystPick");
-	public static final Item AmethystAxe = (new ModItemAxe(5020, toolAmethyst)).setIconCoord(0, 2).setItemName("AmethystAxe");
-	public static final Item AmethystShovel = (new ModItemShovel(5021, toolAmethyst)).setIconCoord(1,2).setItemName("AmethystShovel");
-	public static final Item AmethystSword = (new ModItemSword(5022, toolAmethyst)).setIconCoord(2,2).setItemName("AmethystSword");
-	public static final Item AmethystHoe = (new ModItemHoe(5023, toolAmethyst)).setIconCoord(3,2).setItemName("AmethystHoe");
-	
-	//Armor
-	public static final Item CocoHelmet = (new ModItemCocoArmor(5024, COCO, 6, 0)).setIconCoord(0,3).setItemName("CocoHelmet");
-	public static final Item CocoChest = (new ModItemCocoArmor(5025, COCO, 6, 1)).setIconCoord(1,3).setItemName("CocoChest");
-	public static final Item CocoLegs = (new ModItemCocoArmor(5026, COCO, 6, 2)).setIconCoord(2,3).setItemName("CocoLegs");
-	public static final Item CocoBoots = (new ModItemCocoArmor(5027, COCO, 6, 3)).setIconCoord(3,3).setItemName("CocoBoots");
+		public static final Item AmethystPickaxe = (new ModItemPick(AmethystPickaxeID, toolAmethyst)).setIconCoord(15,1).setItemName("AmethystPick");
+		public static final Item AmethystAxe = (new ModItemAxe(AmethystAxeID, toolAmethyst)).setIconCoord(0, 2).setItemName("AmethystAxe");
+		public static final Item AmethystShovel = (new ModItemShovel(AmethystShovelID, toolAmethyst)).setIconCoord(1,2).setItemName("AmethystShovel");
+		public static final Item AmethystSword = (new ModItemSword(AmethystSwordID, toolAmethyst)).setIconCoord(2,2).setItemName("AmethystSword");
+		public static final Item AmethystHoe = (new ModItemHoe(AmethystHoeID, toolAmethyst)).setIconCoord(3,2).setItemName("AmethystHoe");
+
+		//Armor
+		public static final Item CocoHelmet = (new ModItemCocoArmor(5024, COCO, 6, 0)).setIconCoord(0,3).setItemName("CocoHelmet");
+		public static final Item CocoChest = (new ModItemCocoArmor(5025, COCO, 6, 1)).setIconCoord(1,3).setItemName("CocoChest");
+		public static final Item CocoLegs = (new ModItemCocoArmor(5026, COCO, 6, 2)).setIconCoord(2,3).setItemName("CocoLegs");
+		public static final Item CocoBoots = (new ModItemCocoArmor(5027, COCO, 6, 3)).setIconCoord(3,3).setItemName("CocoBoots");
 
 	@Init
 	public void load(FMLInitializationEvent event)
@@ -249,6 +278,34 @@ public class CocoCraft
 		SilverOreID = Integer.parseInt(config.getOrCreateBlockIdProperty("SilverOreID", 182).value);
 		AmethystOreID = Integer.parseInt(config.getOrCreateBlockIdProperty("AmethystOreID", 183).value);
 		
+		CocoIngotID = config.getOrCreateIntProperty("CocoIngotID", "item", 15000).getInt(15000);
+		MithrilIngotID = config.getOrCreateIntProperty("MithrilIngotID", "item", 15001).getInt(15001);
+		SilverIngotID = config.getOrCreateIntProperty("SilverIngotID", "item", 15002).getInt(15002);
+		AmethystID = config.getOrCreateIntProperty("AmethystGemID", "item", 15003).getInt(15003);
+
+		CocoPickaxeID = config.getOrCreateIntProperty("CocoPickaxeID", "item", 15004).getInt(15004);
+		CocoAxeID = config.getOrCreateIntProperty("CocoAxeID", "item", 15005).getInt(15005);
+		CocoShovelID = config.getOrCreateIntProperty("CocoShovelID", "item", 15006).getInt(15006);
+		CocoSwordID = config.getOrCreateIntProperty("CocoSwordID", "item", 15007).getInt(15007);
+		CocoHoeID = config.getOrCreateIntProperty("CocoHoeID", "item", 15008).getInt(15008);
+
+		MithrilPickaxeID = config.getOrCreateIntProperty("MithrilPickaxeID", "item", 15009).getInt(15009);
+		MithrilAxeID = config.getOrCreateIntProperty("MithrilAxeID", "item", 15010).getInt(15010);
+		MithrilShovelID = config.getOrCreateIntProperty("MithrilShovelID", "item", 15011).getInt(15011);
+		MithrilSwordID = config.getOrCreateIntProperty("MithrilSwordID", "item", 15012).getInt(15012);
+		MithrilHoeID = config.getOrCreateIntProperty("MithrilHoeID", "item", 15013).getInt(15013);
+
+		SilverPickaxeID = config.getOrCreateIntProperty("SilverPickaxeID", "item", 15014).getInt(15014);
+		SilverAxeID = config.getOrCreateIntProperty("SilverAxeID", "item", 15015).getInt(15015);
+		SilverShovelID = config.getOrCreateIntProperty("SilverShovelID", "item", 15016).getInt(15016);
+		SilverSwordID = config.getOrCreateIntProperty("SilverSwordID", "item", 15017).getInt(15017);
+		SilverHoeID = config.getOrCreateIntProperty("SilverHoeID", "item", 15018).getInt(15018);
+
+		AmethystPickaxeID = config.getOrCreateIntProperty("AmethystPickaxeID", "item", 15019).getInt(15019);
+		AmethystAxeID = config.getOrCreateIntProperty("AmethystAxeID", "item", 15020).getInt(15020);
+		AmethystShovelID = config.getOrCreateIntProperty("AmethystShovelID", "item", 15021).getInt(15021);
+		AmethystSwordID = config.getOrCreateIntProperty("AmethystSwordID", "item", 15022).getInt(15022);
+		AmethystHoeID = config.getOrCreateIntProperty("AmethystHoeID", "item", 15023).getInt(15023);
 		
 		config.save();
 		return CocoStoneID;
